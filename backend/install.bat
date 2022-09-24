74cbf4a3b8267a7e5fffad712bb0cecf47167ac7
kuraikara <loris.signoretti@hotmail.it>
2022-09-24 19:57:56 +0200

    Fixed requirements and server install

--- backend/install.bat
+++ backend/install.bat
@@ -1,1 +1,1 @@
-py -m pip install --user virtualenv && py -m venv env && .\env\Scripts\activate && pip install -r requirements.txt && python commands.py
+py -m pip install --user virtualenv && py -m venv env && .\env\Scripts\activate && pip install -r requirements.txt && flask run
